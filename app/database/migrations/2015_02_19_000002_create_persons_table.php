28a3f1b52e6b0048aa3c4674ecb5d6c12b09f173
Tim Jones <tdjones74021@yahoo.com>
2015-04-01 16:17:17 -0500

    Change name of column for e-mail address to 'email_address' to be more descriptive.

--- app/database/migrations/2015_02_19_000002_create_persons_table.php
+++ app/database/migrations/2015_02_19_000002_create_persons_table.php
@@ -18,7 +18,7 @@ class CreatePersonsTable extends Migration {
                         $table->string('bsa_id', 15)->nullable();
                         $table->string('first_name', 20);
                         $table->string('last_name', 30);
-                        $table->string('email', 50)->nullable();
+                        $table->string('email_address', 50)->nullable();
                         $table->string('password')->default('-999');
                         $table->string('home_phone', 20)->nullable();
                         $table->string('cell_phone', 20)->nullable();
