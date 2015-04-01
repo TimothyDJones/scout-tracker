c77a8de68ef605788625ec123dbaeba3ec976556
Tim Jones <tdjones74021@yahoo.com>
2015-04-01 16:16:02 -0500

    Correct relationship to 'Person' model to 'hasMany'. Add "helper" methods for formatting address for display and for showing in 'select' drop-down list.

--- app/models/Address.php
+++ app/models/Address.php
@@ -8,7 +8,7 @@ class Address extends \LaravelBook\Ardent\Ardent {
         
         protected $fillable = array(
             'addr1', 'addr2', 'city', 'state', 'postal_code', 
-            'country', 'person_id',
+            'country', 
         );
         
         protected $guarded = array();
@@ -21,8 +21,8 @@ class Address extends \LaravelBook\Ardent\Ardent {
             'country'           => 'alpha|size:3',
         );
         
-	public function customer() {
-		return $this->belongsTo('Person');
+	public function persons() {
+		return $this->hasMany('Person');
 	}
         
         /**
@@ -46,6 +46,65 @@ class Address extends \LaravelBook\Ardent\Ardent {
         
         public function setCountryAttribute($value) {
             $this->attributes['country'] = strtoupper($value);
+        }
+        
+        /**
+         * Returns an array list of *ALL* addresses
+         * keyed to address ID (primary key) for use in drop-down list.
+         * 
+         * @return array
+         */
+        public static function selectList() {
+            $addresses = array();
+            
+            foreach ( Address::all() as $address ) {
+                $addrLine = $address->addr1 . ', ';
+                $addrLine .= $address->city . ', ' . $address->state . ' ' . $address->postal_code;
+                $addresses[$address->id] = $addrLine;
+            }
+            
+            return $addresses;
+        }
+        
+        
+        /**
+         * Format address as HTML wrapped as paragraph.
+         * 
+         * @param Address $address
+         * @return string
+         */
+        public static function toHtml(Address $address = NULL) {
+            if ( is_null($address) ) {
+                if ( Auth::check() ) {
+                    $address = Auth::user()->address;
+                } else {
+                    return '<p></p>';
+                }
+            }
+            
+            $addr = '<p>' . self::formatAddress($address, '<br />') . '</p>';
+            
+            return $addr;
+        }
+        
+        /**
+         * Format address for printing or display, including line breaks.
+         * 
+         * @param Address $address
+         * @param string $separator
+         * @return string
+         */
+        public static function formatAddress(Address $address, $separator = PHP_EOL) {
+            $addr = $address->addr1 . $separator;
+            if ( strlen($address->addr2) ) {
+                $addr .= $address->addr2 . $separator;
+            }
+            $addr .= $address->city . ', ' . $address->state . ' ' . $address->postal_code . $separator;
+            if ( strlen($address->country) ) {
+                $addr .= $address->country . $separator;
+            }
+            
+            return $addr;
         }
         
         
