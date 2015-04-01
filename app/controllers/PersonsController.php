2d237d2b44179f2915934ba79b498694f7950436
Tim Jones <tdjones74021@yahoo.com>
2015-04-01 16:09:55 -0500

    Implement 'create' and 'store' methods.

--- app/controllers/PersonsController.php
+++ app/controllers/PersonsController.php
@@ -1,12 +1,12 @@
 <?php
 
-use storage\person\PersonRepository as Person;
+//use storage\person\PersonRepository as Person;
 
 class PersonsController extends \BaseController {
     
-        public function __construct(Person $person) {
-            $this->person = $person;
-        }
+        //public function __construct(Person $person) {
+        //    $this->person = $person;
+        //}
 
 	/**
 	 * Display a listing of the resource.
@@ -15,8 +15,8 @@ class PersonsController extends \BaseController {
 	 */
 	public function index()
 	{
-            return $this->person->all();
-            Log::debug("person->all(): " . print_r($this->person->all(), TRUE));
+            //return $this->person->all();
+            return Person::all();
 	}
 
 
@@ -27,7 +27,13 @@ class PersonsController extends \BaseController {
 	 */
 	public function create($personClass = 'Scout')
 	{
-            $this->layout->content = View::make('persons.create', array('class_name' => $personClass, 'updateFlag' => FALSE));
+            // Get list of addresses to display.
+            $addresses = Address::selectList();
+            $this->layout->content = View::make('persons.create', 
+                    array('class_name' => $personClass, 
+                        'updateFlag' => FALSE, 
+                        'addressList' => $addresses)
+                    );
 	}
 
 
@@ -38,7 +44,54 @@ class PersonsController extends \BaseController {
 	 */
 	public function store()
 	{
-		//
+            $input = array_except(Input::all(), array('class_name'));
+            
+            // We must has *BOTH* 'password' and 'password_confirmation' so that validator works!
+            //$input['password'] = Hash::make($input['password']);
+            //$input['password_confirmation'] = Hash::make($input['password_confirmation']);
+            
+            $validation_rules = Person::$validation_rules;
+            // If the currently logged in user is admin, then password
+            // verification is not required.
+            if ( Utility::isAdminUser() ) {
+                $validation_rules = array_except(Person::$validation_rules, array('password', 'password_confirmation'));
+            }
+            $validator = Validator::make($input, $validation_rules);
+            
+            if ( $validator->passes() ) {
+                unset($input['password_confirmation']);     // Remove password confirmation field from array.
+                
+                // Hash password before saving.
+                $input['password'] = Hash::make($input['password']);
+                
+                $class_name = Input::get('class_name');
+                switch ($class_name) {
+                    case 'Adult':
+                        $person = new Adult($input);
+                        break;
+                    case 'Scout':
+                        $person = new Scout($input);
+                        break;
+                    default:        // 'Scout' is default.
+                        $input['class_name'] = 'Scout';
+                        $person = new Scout($input);
+                }                
+                
+                // We "force" the save, because we have already done validation above.
+                if ( $person->forceSave() ) {
+                    $message = 'New ' . $class_name . ' "' . $person->first_name . ' ' . $person->last_name . '" created.';
+                    return Redirect::route('profile', array($person->id))->with(array('message' => $message));
+                }
+            } else {  // If validation fails, redirect to previous page.
+                return Redirect::route('persons.create')
+                        ->withInput()
+                        ->withErrors( $validator->errors() )
+                        ->with(array('message' => 'Validation error.', 'updateFlag' => FALSE));                
+            }
+            
+
+            
+
 	}
 
 
@@ -48,7 +101,7 @@ class PersonsController extends \BaseController {
 	 * @param  int  $id
 	 * @return Response
 	 */
-	public function show($id)
+	public function show(Person $person)
 	{
 		//
 	}
