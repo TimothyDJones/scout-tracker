c692160acf1160391afc245d90ea5b05433fd85f
Tim Jones <tdjones74021@yahoo.com>
2015-04-01 16:10:44 -0500

    Add hidden field to store the "class name" (Scout or Adult) to determine type of person to create.

--- app/views/persons/create.blade.php
+++ app/views/persons/create.blade.php
@@ -15,7 +15,8 @@
         <h2>{{ $page_title or "Create " . $class_name }}</h2>
     </div>
 
-        {{ Form::model(new Scout, array('route' => ['scouts.store'], 'role' => 'form', 'class' => 'form-horizontal')) }}
+        {{ Form::model(new Person, array('route' => ['persons.store'], 'role' => 'form', 'class' => 'form-horizontal')) }}
+            {{ Form::hidden('class_name', $class_name) }}
             @include('persons/partials/_form', array('submit_button_label' => 'Create'))
 	{{ Form::close() }}
 
