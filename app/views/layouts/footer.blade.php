b908840c0e2c179022a6558f3ffdc89a4581104e
Tim Jones <tdjones74021@yahoo.com>
2015-04-02 17:36:27 -0500

    Add new CSS and updatefooter navbar.

--- app/views/layouts/footer.blade.php
+++ app/views/layouts/footer.blade.php
@@ -1,20 +1,16 @@
 @section("footer")
 <footer>
-    <div class="footer">
-        <div class="container">
-            <div class="row">
-                {{--
-                <div class="col-xs-4">
-                    {{ link_to_route('items.index', 'Free Downloads', NULL, array('class' => 'text-center')) }}
-                <div class="col-xs-4">
-                    {{ link_to_route('products.index', 'Order CDs, DVDs, and MP3s', NULL, array('class' => 'text-center')) }}
-                </div>
-                --}}
-                <div class="col-xs-12">
-                    &copy;&nbsp;2008&nbsp;-&nbsp;<?php echo date("Y"); ?> <strong>{{ HTML::link('http://www.workshopmultimedia.com', 'Workshop Multimedia', array('id' => 'wmmUrl', 'class' => 'text-center')); }}</strong>
-                </div>
-            </div>
-        </div>
-    </div> 
+    <div class="subnav-wrap">
+            <div class="container">
+                    <div class="row subnav">
+                            <div class="col-xs-12">
+                                    <div class="text-center">
+                                        {{ link_to_route('login', 'Log In') }}
+                                        {{ link_to_route('profile', 'Profile') }}
+                                    </div>
+                            </div>
+                    </div><!-- row -->
+            </div><!-- container -->
+    </div><!-- subnav-wrap -->
 </footer>
 @show
--- app/views/layouts/partials/_assets.blade.php
+++ app/views/layouts/partials/_assets.blade.php
@@ -2,5 +2,6 @@
         {{ HTML::style('css/font-awesome.min.css') }}
         {{ HTML::style('//fonts.googleapis.com/css?family=Open+Sans') }}
         {{ HTML::style('css/layout.css') }}
+        {{ HTML::style('css/main.css') }}
 	{{ HTML::script('js/jquery-1.11.1.min.js') }}
         {{ HTML::script('js/bootstrap.min.js') }}
