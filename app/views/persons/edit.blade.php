2a6057e902aa077496305a2e04f9091998fc453a
Tim Jones <tdjones74021@yahoo.com>
2015-04-06 14:27:55 -0500

    Add missing closing '}}' on blade template.

--- app/views/persons/edit.blade.php
+++ app/views/persons/edit.blade.php
@@ -2,7 +2,7 @@
 	<h2>{{ $page_title or "Edit Scout" }}</h2>
 
         {{ Form::model($person, array('method' => 'PATCH', 'route' => array('persons.update', $customer->id), 'role' => 'form', 'class' => 'form-horizontal')) }}
-            {{ Form::hidden('class_name', $person->class_name)
+            {{ Form::hidden('class_name', $person->class_name) }}
             @include('persons/partials/_form', array('submit_button_label' => 'Update'))
 	{{ Form::close() }}
 
