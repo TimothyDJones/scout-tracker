ce83126953c1b4040c810f80b40b1300a370e64c
Tim Jones <tdjones74021@yahoo.com>
2015-04-06 16:55:13 -0500

    Change display of scouts and adults to embedded tables for better layout.

--- app/views/persons/index.blade.php
+++ app/views/persons/index.blade.php
@@ -3,44 +3,71 @@
     
     <div class="row">
         <div class="col-md-6 col-xs-6">
-            <h3>Scouts</h3>
-            <div class="pull-right">
-                {{ link_to_route('persons.create', 'New', array('class_name' => 'Scout'), array('class' => 'btn btn-primary btn-small')) }}
-            </div>
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th><h3>Scouts</h3></th>
+                                        <th>
+                                            <div class="pull-right">
+                                                {{ link_to_route('persons.create', 'New', array('class_name' => 'Scout'), array('class' => 'btn btn-primary btn-small')) }}
+                                            </div>                                        
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ( $persons as $person )
+                                    <tr>
+                                        @if ( $person->class_name == 'Scout' )
+                                            <td>
+                                                {{ $person->first_name }}&nbsp;{{ $person->last_name}}
+                                            </td>
+                                            <td>
+                                                {{ link_to_route('profile', 'Details', array('id' => $person->id), array('class' => 'btn btn-info btn-sm')) }}&nbsp;
+                                                {{ link_to_route('persons.edit', 'Edit', array('id' => $person->id), array('class' => 'btn btn-primary btn-sm')) }}
+                                            </td>
+                                        @endif
+                                    </tr>
+                                @endforeach                                    
+                                </tbody>
+                            </table>
+                        </div>
+            
+
         </div>
         <div class="col-md-6 col-xs-6">
-            <h3>Adults</h3>
-            <div class="pull-right">
-                {{ link_to_route('persons.create', 'New', array('class_name' => 'Adult'), array('class' => 'btn btn-primary btn-small')) }}
-            </div>            
-        </div>
-    </div>
-    
-    <div class="row">
-        <div class="col-md-6 col-xs-6">
-            @foreach ( $persons as $person )
-                @if ( $person->class_name == 'Scout' )
-                <p>
-                {{ $person->first_name }}&nbsp;{{ $person->last_name}}
-                {{ link_to_route('profile', 'Details', array('id' => $person->id), array('class' => 'btn btn-info btn-sm')) }}
-                {{ link_to_route('persons.edit', 'Edit', array('id' => $person->id), array('class' => 'btn btn-primary btn-sm')) }}
-                </p>
-                @endif
-            @endforeach
-        </div>
-        <div class="col-md-6 col-xs-6">
-            @foreach ( $persons as $person )
-                @if ( $person->class_name == 'Adult' )
-                <p>
-                {{ $person->first_name }}&nbsp;{{ $person->last_name}}
-                @if ( $person->admin_ind == TRUE )
-                *
-                @endif
-                {{ link_to_route('profile', 'Details', array('id' => $person->id), array('class' => 'btn btn-info btn-sm')) }}
-                {{ link_to_route('persons.edit', 'Edit', array('id' => $person->id), array('class' => 'btn btn-primary btn-sm')) }}
-                </p>
-                @endif
-            @endforeach            
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th><h3>Adults</h3></th>
+                                        <th>
+                                            <div class="pull-right">
+                                                {{ link_to_route('persons.create', 'New', array('class_name' => 'Adult'), array('class' => 'btn btn-primary btn-small')) }}
+                                            </div>                                        
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @foreach ( $persons as $person )
+                                    <tr>
+                                        @if ( $person->class_name == 'Adult' )
+                                            <td>
+                                                {{ $person->first_name }}&nbsp;{{ $person->last_name}}
+                                                @if ( $person->admin_ind == TRUE )
+                                                <strong>*</strong>
+                                                @endif                                            
+                                            </td>
+                                            <td>
+                                                {{ link_to_route('profile', 'Details', array('id' => $person->id), array('class' => 'btn btn-info btn-sm')) }}&nbsp;
+                                                {{ link_to_route('persons.edit', 'Edit', array('id' => $person->id), array('class' => 'btn btn-primary btn-sm')) }}
+                                            </td>
+                                        @endif
+                                    </tr>
+                                @endforeach                                    
+                                </tbody>
+                            </table>
+                        </div>
         </div>
     </div>
 @stop
