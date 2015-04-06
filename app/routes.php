64346c70121946860aa6f8d7706fb4c9cc6bfc14
Tim Jones <tdjones74021@yahoo.com>
2015-04-06 16:30:32 -0500

    Add routes for addresses and person login.

--- app/routes.php
+++ app/routes.php
@@ -19,14 +19,19 @@ Route::get('/', function()
 Route::model('persons', 'Person');
 Route::model('scouts', 'Scout');
 Route::model('adults', 'Adult');
+Route::model('addresses', 'Address');
 
-Route::get('persons/login', array('as' => 'login', 'uses' => 'PersonsController@login'));
+Route::get('login', array('as' => 'login', 'uses' => 'PersonsController@login'))->before('guest');
+Route::get('persons/login', array('as' => 'login', 'uses' => 'PersonsController@login'))->before('guest');
 Route::post('persons/authenticate', array('as' => 'authenticate', 'uses' => 'PersonsController@authenticate'));
 Route::get('profile/{persons?}', array('as' => 'profile', 'uses' => 'PersonsController@show'));
 Route::get('persons/show/{persons?}', array('as' => 'profile', 'uses' => 'PersonsController@show'));
+Route::get('persons/changePassword/(persons?)', array('as' => 'changePassword', 'uses' => 'PersonsController@changePassword'))->before('auth');
+Route::post('persons/updatePassword/(persons)', array('as' => 'updatePassword', 'uses' => 'PersonsController@updatePassword'))->before('auth');
 Route::get('scouts/court-of-honor', array('as' => 'court-of-honor', 'uses' => 'ScoutsController@coh'));
 Route::get('scouts/search', array('as' => 'search', 'uses' => 'ScoutsController@search'));
 
 Route::resource('persons', 'PersonsController');
 Route::resource('scouts', 'ScoutsController');
 Route::resource('adults', 'AdultsController');
+Route::resource('addresses', 'AddressesController');
