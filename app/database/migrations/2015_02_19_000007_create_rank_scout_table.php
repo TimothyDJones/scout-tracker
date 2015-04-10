3f09efa0d513b7e6c8859c80b0848fd19ede3988
Tim Jones <tdjones74021@yahoo.com>
2015-04-10 14:12:52 -0500

    Add migration for 'rank_scout' pivot table.

---
+++ app/database/migrations/2015_02_19_000007_create_rank_scout_table.php
@@ -0,0 +1,50 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateRankScoutTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+                Schema::create('rank_scout', function(Blueprint $table) {
+                        $table->increments('id');
+                        $table->integer('rank_id');
+                        $table->foreign('rank_id')
+                                ->references('id')
+                                ->on('awards')
+                                ->onDelete('cascade')
+                                ->onUpdate('cascade');                             
+                        $table->integer('scout_id');
+                        $table->foreign('scout_id')
+                                ->references('id')
+                                ->on('persons')
+                                ->onDelete('cascade')
+                                ->onUpdate('cascade');     
+                        $table->date('rank_awarded_date')->nullable();
+                });                
+                
+                // Re-enable FK constraints...  Just in case!  :)
+                DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+                
+	}
+
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+                DB::statement('SET FOREIGN_KEY_CHECKS = 0');
+                Schema::dropIfExists('rank_scout');
+                DB::statement('SET FOREIGN_KEY_CHECKS = 1');
+	}
+
+}
