513d0fdf974dbc30431553515d31b36d86d07d25
Tim Jones <tdjones74021@yahoo.com>
2015-04-13 17:20:45 -0500

    Add new Bootstrap theme style sheet.

--- app/views/layouts/partials/_assets.blade.php
+++ app/views/layouts/partials/_assets.blade.php
@@ -3,5 +3,6 @@
         {{ HTML::style('//fonts.googleapis.com/css?family=Open+Sans') }}
         {{ HTML::style('css/layout.css') }}
         {{ HTML::style('css/main.css') }}
+        {{ HTML::style('css/newStyles.css') }}
 	{{ HTML::script('js/jquery-1.11.1.min.js') }}
         {{ HTML::script('js/bootstrap.min.js') }}
