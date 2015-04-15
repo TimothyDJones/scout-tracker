765cf24213136c981be0534c1561c392db8b4d94
Tim Jones <tdjones74021@yahoo.com>
2015-04-15 17:22:09 -0500

    Add basic structure for editing or inserting new 'award_scout' pivot table record for rank record.

--- app/views/scouts/ranks/edit.blade.php
+++ app/views/scouts/ranks/edit.blade.php
@@ -8,5 +8,42 @@
             {{ Form::hidden('class_name', $class_name) }}
             @include('persons/partials/_form', array('submit_button_label' => 'Create'))
 	{{ Form::close() }}
+        
+    <div class="row">
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                    <tr>
+                                        <th>{{ $award_type }}</th>
+                                        <th>Status</th>
+                                        <th>SM Conf. Date</th>
+                                        <th>BOR Date</th>
+                                        <th>Date Completed</th>
+                                        <th>Approved By</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach( $rankList as $rank )
+                                    <tr>
+                                    @if ( !is_null( $rank->pivot->id) )
+                                        // We have an existing pivot table row...
+                                        // Use Form::model(...)
+                                        <td>{{ $rank->award_name }}</td>
+                                        <td>{{ $rank->pivot->award_status or 'N/A' }}</td>
+                                        <td>{{ $rank->pivot->date_sm_conf or 'N/A' }}</td>
+                                        <td>{{ $rank->pivot->date_board_of_review or 'N/A' }}</td>
+                                        <td>{{ $rank->pivot->date_completed or 'N/A' }}</td>
+                                        <td>{{ $rank->pivot->approver_last_name or 'N/A' }}&nbsp;{{ $rank->pivot->approver_first_name or '' }}</td>
+                                    
+                                    @else
+                                        // We need to create a new pivot table row...
+                                        // Use Form::open(...)
+                                    @endif
+                                    </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>
+    </div>        
 
 @stop
