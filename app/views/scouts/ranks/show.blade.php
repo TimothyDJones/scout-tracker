3c0384698827c5dceda08e7d651fcab108aaebb2
Tim Jones <tdjones74021@yahoo.com>
2015-04-15 17:05:45 -0500

    Update scout rank view to display new columns, such as SM conf. date and Board of Review date.

--- app/views/scouts/ranks/show.blade.php
+++ app/views/scouts/ranks/show.blade.php
@@ -3,6 +3,7 @@
     <div class="row">
         <h2>{{ $award_type . " Advancement History for " . $scout->first_name . ' ' . $scout->last_name }}</h2>
         <h3>Current Rank: <strong>{{ $scout->currentRank()->award_name }}</strong></h3>
+        {{ Kint::dump($rankList) }}
     </div>
 
     <div class="row">
@@ -11,6 +12,9 @@
                                 <thead>
                                     <tr>
                                         <th>{{ $award_type }}</th>
+                                        <th>Status</th>
+                                        <th>SM Conf. Date</th>
+                                        <th>BOR Date</th>
                                         <th>Date Completed</th>
                                         <th>Approved By</th>
                                     </tr>
@@ -19,6 +23,9 @@
                                     @foreach( $rankList as $rank )
                                     <tr>
                                         <td>{{ $rank->award_name }}</td>
+                                        <td>{{ $rank->pivot->award_status or 'N/A' }}</td>
+                                        <td>{{ $rank->pivot->date_sm_conf or 'N/A' }}</td>
+                                        <td>{{ $rank->pivot->date_board_of_review or 'N/A' }}</td>
                                         <td>{{ $rank->pivot->date_completed or 'N/A' }}</td>
                                         <td>{{ $rank->pivot->approver_last_name or 'N/A' }}&nbsp;{{ $rank->pivot->approver_first_name or '' }}</td>
                                     </tr>
