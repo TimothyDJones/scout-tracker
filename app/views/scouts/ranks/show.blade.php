ce50ea154083ae10bce732a3de21d020cdd46fe4
Tim Jones <tdjones74021@yahoo.com>
2015-04-24 15:42:44 -0500

    Make scout name a link to person details page.

--- app/views/scouts/ranks/show.blade.php
+++ app/views/scouts/ranks/show.blade.php
@@ -1,7 +1,8 @@
 @section('main')
 
     <div class="row">
-        <h2>{{ $award_type . " Advancement History for " . $scout->first_name . ' ' . $scout->last_name }}</h2>
+        <h2>{{ $award_type . " Advancement History for " }}
+            {{ link_to_route('persons.show', $scout->first_name . ' ' . $scout->last_name, array('id' => $scout->id)) }}</h2>
         <h3>Current Rank: <strong>{{ $scout->currentRank()->award_name }}</strong></h3>
         {{ Kint::dump($rankList) }}
     </div>
