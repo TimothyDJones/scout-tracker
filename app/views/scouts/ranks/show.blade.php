92c31c63427c542276d382f419fad3ff911346d3
Tim Jones <tdjones74021@yahoo.com>
2015-04-24 15:54:40 -0500

    Move page heading for advancement to common 'partial' blade template.

--- app/views/scouts/ranks/show.blade.php
+++ app/views/scouts/ranks/show.blade.php
@@ -1,11 +1,6 @@
 @section('main')
 
-    <div class="row">
-        <h2>{{ $award_type . " Advancement History for " }}
-            {{ link_to_route('persons.show', $scout->first_name . ' ' . $scout->last_name, array('id' => $scout->id)) }}</h2>
-        <h3>Current Rank: <strong>{{ $scout->currentRank()->award_name }}</strong></h3>
-        {{ Kint::dump($rankList) }}
-    </div>
+    @include('scouts/partials/_award_heading')
 
     <div class="row">
                         <div class="table-responsive">
